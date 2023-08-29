f4f7bab89a6beff5556815e09fc04d13a85413d7
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-08-29 13:51:07 +0530

    star click

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
-import StarRating from './start';
+import StarRating from './star';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
