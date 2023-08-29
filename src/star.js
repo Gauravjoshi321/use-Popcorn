cdfd8765acef1285c30e8c2e766ea4c3c4ebaee7
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-08-29 16:47:16 +0530

    dynamic stars

--- src/star.js
+++ src/star.js
@@ -1,4 +1,3 @@
-import { hover } from "@testing-library/user-event/dist/hover";
 import { useState } from "react"
 
 const containerStyle = {
@@ -19,18 +18,22 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
+            key={i + 1}
             onClick={() => setRating(i + 1)}
-            full={rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   )
 }
@@ -42,10 +45,17 @@ const styleStar = {
   display: "block",
 }
 
-function Star({ onClick, full }) {
+function Star({ onClick, onHoverIn, onHoverOut, full }) {
   return (
     // FULL STAR
-    <span role="button" style={styleStar} onClick={onClick}>
+    <span
+      role="button"
+      style={styleStar}
+      onClick={onClick}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
+
       {full
         ? < svg
           xmlns="http://www.w3.org/2000/svg"
