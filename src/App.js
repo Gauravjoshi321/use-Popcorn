f02fa7738af35297bb117ef3e33fb03448225167
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2023-09-06 14:03:44 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,15 +50,18 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "2d1e77c8"
+const KEY = "2d1e77c8";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-    .then(res => res.json())
-    .then(data => console.log(data));
+  useEffect(function () {
+
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=singh is king`)
+      .then(res => res.json())
+      .then(data => setMovies(data.Search));
+  }, [])
 
   return (
     <>
